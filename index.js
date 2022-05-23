beadc11aba66676a3648a47c2c643e11e0c6fb48
SarafNusrat <saraf.nusrat11@gmail.com>
2022-05-24 04:47:58 +0600

    secret access token added and project done

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
+
+// function verifyJWT(req, res, next){
+//     const authHeader = req.headers.authorization;
+//     next();
+//     if (!authHeader){
+//         return res.status(401).send({message: 'unauthorized access'});
+//     }
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//         if(err) {
+//             console.log('error');
+//             return res.status(403).send( {message: 'Forbidden access'});
+//         }
+//         console.log(token);
+//         req.decoded = decoded;
+//         console.log('decoded', decoded);
+//         next();
+//     })
+// }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sdso6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -16,6 +37,15 @@ async function run() {
     try{
         await client.connect();
         const itemCollection = client.db('veggieMart').collection('item');
+
+        // AUTH
+        app.post('/login', async (req,res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '2d'
+            });
+            res.send({accessToken});
+        })
 
         app.get('/item', async(req,res) => {
           const query = {};
@@ -60,12 +90,19 @@ async function run() {
             res.send(result);
         })
 
+        // Get My Items
         app.get('/myItem', async(req,res) => {
+            // const decodedEmail = req.decoded.email; 
             const email = req.query.email;
-            const query = {email: email};
-            const cursor = itemCollection.find(query);
-            const items = await cursor.toArray();
-            res.send(items);
+            // if (email === decodedEmail) {
+                const query = {email: email};
+                const cursor = itemCollection.find(query);
+                const items = await cursor.toArray();
+                res.send(items);
+            // }
+            // else {
+            //     res.status(403).send({message: 'forbidden access'})
+            // }   
         })
     }
     finally{
